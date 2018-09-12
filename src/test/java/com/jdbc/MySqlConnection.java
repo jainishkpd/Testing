976e5a794f1780fc40e4894acd832ff8d3e55807
Jainish Kapadia <jkapadia@blackberry.com>
2018-09-12 11:47:29 +0530

    Add JDBC Code

---
+++ src/test/java/com/jdbc/MySqlConnection.java
@@ -0,0 +1,32 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.testng.annotations.Test;
+
+public class MySqlConnection {
+	
+	@Test
+	public void dbConnection() throws ClassNotFoundException, SQLException {
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		System.out.println("Driver loaded");
+
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdummy","root","root");
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery("select e.Id, e.name, e.email, e.city, s.gender, s.Ammount from \r\n" + 
+				"employee e\r\n" + 
+				"inner join salary s on e.name = s.name\r\n" + 
+				"WHERE s.gender = 'female' AND s.Ammount > 65000");
+		
+		while(rs.next()) {
+			System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5));
+		}	
+		con.close();
+	   }
+
+}
