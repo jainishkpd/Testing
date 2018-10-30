6f90cbb7a58de3914a46f8c8fb00b0ab2f92d0bb
Jainish Kapadia <jainishkpd@gmail.com>
2018-10-30 12:36:04 +0530

    Update file

--- src/test/java/com/jdbc/MySqlConnection.java
+++ src/test/java/com/jdbc/MySqlConnection.java
@@ -5,28 +5,28 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+import java.util.logging.Logger;
 import org.testng.annotations.Test;
 
 public class MySqlConnection {
-	
+
 	@Test
 	public void dbConnection() throws ClassNotFoundException, SQLException {
-		
+
 		Class.forName("com.mysql.jdbc.Driver");
-		System.out.println("Driver loaded");
+		Logger.getLogger("Driver loaded");
 
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdummy","root","root");
-		Statement stmt = con.createStatement();
-		ResultSet rs = stmt.executeQuery("select e.Id, e.name, e.email, e.city, s.gender, s.Ammount from \r\n" + 
-				"employee e\r\n" + 
-				"inner join salary s on e.name = s.name\r\n" + 
-				"WHERE s.gender = 'female' AND s.Ammount > 65000");
-		
-		while(rs.next()) {
-			System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5));
-		}	
-		con.close();
-	   }
-
+		try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdummy","root","root");
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery("select e.Id, e.name, e.email, e.city, s.gender, s.Ammount from \r\n" + 
+						"employee e\r\n" + 
+						"inner join salary s on e.name = s.name\r\n" + 
+						"WHERE s.gender = 'female' AND s.Ammount > 65000");) {
+			while(rs.next()) {
+				System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5));
+			}	
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
 }
